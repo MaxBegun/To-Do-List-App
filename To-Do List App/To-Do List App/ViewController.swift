8f0e3f09fbe7d60c4d19276fe70a60097d2cd01b
Maksim Begun <maksbegun@icloud.com>
2023-08-20 20:30:17 +0300

    Add navigation controller

--- To-Do List App/To-Do List App/ViewController.swift
+++ To-Do List App/To-Do List App/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  To-Do List App
-//
-//  Created by Maksim Begun on 20.08.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
