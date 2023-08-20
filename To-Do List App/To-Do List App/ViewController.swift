c1a0c0944c364d90930b98f8320378ccd4293126
Maksim Begun <maksbegun@icloud.com>
2023-08-20 20:23:05 +0300

    Initial commit

---
+++ To-Do List App/To-Do List App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do List App
+//
+//  Created by Maksim Begun on 20.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
